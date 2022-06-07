da5376ee5d7df08de906c2b9c84c3c159830a172
Your Name <you@example.com>
2022-06-07 08:41:42 +0600

    example data add for checking

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route exact path="/" element={<Home></Home>} />
+        <Route exact path ="/" element={<Home></Home>} />
         <Route path="/home" element={<Home></Home>} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/studentRegister" element={<StudentRegister />} />
